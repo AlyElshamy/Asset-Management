bc8def2eb84a9fe372d906905af7755503b4ddb2
Ayamahmoud359 <101875618+Ayamahmoud359@users.noreply.github.com>
2022-04-30 23:23:55 +0200

    Asset Lost && Dispose Asset

---
+++ Models/AssetLost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetProject.Models
+{
+    public class AssetLost
+    {
+        public int AssetLostId { get; set; }
+        public int AssetId { get; set; }
+        public Asset Asset { set; get; }
+        [Column(TypeName = "date")]
+        public DateTime DateLost { get; set; }
+        public string Notes { get; set; }
+    }
+}
