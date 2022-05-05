305f54c443f63e212cc44ff5c0ac43c10825a000
Ayamahmoud359 <101875618+Ayamahmoud359@users.noreply.github.com>
2022-05-05 18:17:00 +0200

    Reporting

---
+++ ReportModels/FilterModel.cs
@@ -0,0 +1,26 @@
+﻿using AssetProject.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AssetProject.ReportModels
+{
+    public class FilterModel
+    {
+        public DateTime? FromDate { set; get; }
+        public DateTime? ToDate { set; get; }
+
+        public string AssetTagId { set; get; }
+        public double AssetCost { set; get; }
+        public string AssetSerialNo { set; get; }
+        public DateTime? AssetPurchaseDate { set; get; }
+        public Item Item { set; get; }
+        public int? ItemId { set; get; }
+        public DepreciationMethod DepreciationMethod { set; get; }
+        public int? DepreciationMethodId { set; get; }
+        public Vendor Vendor { set; get; }
+        public int? VendorId  { set; get; }
+    }
+}
