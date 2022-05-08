92b90317ae5ff6db6650f4f8669b11b819ad0508
AyaMostafa5 <ayamostafa5@gmail.com>
2022-05-08 13:57:42 +0200

    DataSeeding

--- Data/AssetContext.cs
+++ Data/AssetContext.cs
@@ -25,8 +25,70 @@ namespace AssetProject.Data
             modelBuilder.Entity<ActionType>().HasData(new ActionType { ActionTypeId = 1, ActionTypeTitle = "To Employee" });
             modelBuilder.Entity<ActionType>().HasData(new ActionType { ActionTypeId = 2, ActionTypeTitle = "To Department" });
 
+            //Maintainance Frequency
+            modelBuilder.Entity<AssetMaintainanceFrequency>().HasData(new AssetMaintainanceFrequency { AssetMaintainanceFrequencyId = 1, AssetMaintainanceFrequencyTitle= "Daily" });
+            modelBuilder.Entity<AssetMaintainanceFrequency>().HasData(new AssetMaintainanceFrequency { AssetMaintainanceFrequencyId = 2, AssetMaintainanceFrequencyTitle = "Weekly" });
+            modelBuilder.Entity<AssetMaintainanceFrequency>().HasData(new AssetMaintainanceFrequency { AssetMaintainanceFrequencyId = 3, AssetMaintainanceFrequencyTitle = "Monthly" });
+            modelBuilder.Entity<AssetMaintainanceFrequency>().HasData(new AssetMaintainanceFrequency { AssetMaintainanceFrequencyId = 4, AssetMaintainanceFrequencyTitle = "Yearly" });
+
+            //Asset Movement Direction 
+            modelBuilder.Entity<AssetMovementDirection>().HasData(new AssetMovementDirection { AssetMovementDirectionId = 1, AssetMovementDirectionTitle = "CheckOut" });
+            modelBuilder.Entity<AssetMovementDirection>().HasData(new AssetMovementDirection { AssetMovementDirectionId = 2, AssetMovementDirectionTitle = "CheckIn" });
+
+            //Asset Statuses
+            modelBuilder.Entity<AssetStatus>().HasData(new AssetStatus { AssetStatusId = 1, AssetStatusTitle = "CheckedIn(Avaliable)" });
+            modelBuilder.Entity<AssetStatus>().HasData(new AssetStatus { AssetStatusId = 2, AssetStatusTitle = "CheckedOut" });
+            modelBuilder.Entity<AssetStatus>().HasData(new AssetStatus { AssetStatusId = 3, AssetStatusTitle = "InRepair" });
+            modelBuilder.Entity<AssetStatus>().HasData(new AssetStatus { AssetStatusId = 4, AssetStatusTitle = "Lost" });
+            modelBuilder.Entity<AssetStatus>().HasData(new AssetStatus { AssetStatusId = 5, AssetStatusTitle = "Disposed" });
+            modelBuilder.Entity<AssetStatus>().HasData(new AssetStatus { AssetStatusId = 6, AssetStatusTitle = "Leased" });
+            modelBuilder.Entity<AssetStatus>().HasData(new AssetStatus { AssetStatusId = 7, AssetStatusTitle = "Sold" });
+            modelBuilder.Entity<AssetStatus>().HasData(new AssetStatus { AssetStatusId = 8, AssetStatusTitle = "Broken" });
+            modelBuilder.Entity<AssetStatus>().HasData(new AssetStatus { AssetStatusId = 9, AssetStatusTitle = "InMaimtainance" });
+
+            //Months
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 1,MonthTitle = "January" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 2, MonthTitle = "February" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 3, MonthTitle = "March" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 4, MonthTitle = "April" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 5, MonthTitle = "May" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 6, MonthTitle = "June" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 7, MonthTitle = "July" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 8, MonthTitle = "August" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 9, MonthTitle = "September" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 10, MonthTitle = "October" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 11, MonthTitle = "November" });
+            modelBuilder.Entity<Month>().HasData(new Month { MonthId = 12, MonthTitle = "December" });
+
+            //Week Days
+            modelBuilder.Entity<WeekDay>().HasData(new WeekDay { WeekDayId = 1, WeekDayTitle = "Saturday" });
+            modelBuilder.Entity<WeekDay>().HasData(new WeekDay { WeekDayId = 2, WeekDayTitle = "Sunday" });
+            modelBuilder.Entity<WeekDay>().HasData(new WeekDay { WeekDayId = 3, WeekDayTitle = "Monday" });
+            modelBuilder.Entity<WeekDay>().HasData(new WeekDay { WeekDayId = 4, WeekDayTitle = "Tuesday" });
+            modelBuilder.Entity<WeekDay>().HasData(new WeekDay { WeekDayId = 5, WeekDayTitle = "Wednesday" });
+            modelBuilder.Entity<WeekDay>().HasData(new WeekDay { WeekDayId = 6, WeekDayTitle = "Thursday" });
+            modelBuilder.Entity<WeekDay>().HasData(new WeekDay { WeekDayId = 7, WeekDayTitle = "Friday" });
 
 
+            //Action Logs
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 1, ActionLogTitle = "Asset Purchase" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 2, ActionLogTitle = "Creation Link Contract" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 3, ActionLogTitle = "Creation Link Insurance" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 4, ActionLogTitle = "Creation Link Document" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 5, ActionLogTitle = "Add New Asset Photo" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 6, ActionLogTitle = "Dettached Asset Contract" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 7, ActionLogTitle = "Dettached Asset Insurance" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 8, ActionLogTitle = "Dettached Asset Document" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 9, ActionLogTitle = "Delete Asset Photo" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 10, ActionLogTitle = "Sell Asset" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 11, ActionLogTitle = "Dispose Asset " });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 12, ActionLogTitle = "Broken Asset" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 13, ActionLogTitle = "Repair Asset" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 14, ActionLogTitle = "Repair Asset" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 15, ActionLogTitle = "Asset Leasing" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 16, ActionLogTitle = "CheckIn" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 17, ActionLogTitle = "CheckOut" });
+            modelBuilder.Entity<ActionLog>().HasData(new ActionLog { ActionLogId = 18, ActionLogTitle = "Asset Maintainance" });
         }
 
 
---
+++ Migrations/Asset/20220508115442_DataSeeding2.cs
@@ -0,0 +1,374 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AssetProject.Migrations.Asset
+{
+    public partial class DataSeeding2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ActionLogs",
+                columns: new[] { "ActionLogId", "ActionLogTitle" },
+                values: new object[,]
+                {
+                    { 1, "Asset Purchase" },
+                    { 18, "Asset Maintainance" },
+                    { 17, "CheckOut" },
+                    { 16, "CheckIn" },
+                    { 15, "Asset Leasing" },
+                    { 13, "Repair Asset" },
+                    { 12, "Broken Asset" },
+                    { 11, "Dispose Asset " },
+                    { 10, "Sell Asset" },
+                    { 14, "Repair Asset" },
+                    { 8, "Dettached Asset Document" },
+                    { 7, "Dettached Asset Insurance" },
+                    { 6, "Dettached Asset Contract" },
+                    { 5, "Add New Asset Photo" },
+                    { 4, "Creation Link Document" },
+                    { 3, "Creation Link Insurance" },
+                    { 2, "Creation Link Contract" },
+                    { 9, "Delete Asset Photo" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AssetMaintainanceFrequencies",
+                columns: new[] { "AssetMaintainanceFrequencyId", "AssetMaintainanceFrequencyTitle" },
+                values: new object[,]
+                {
+                    { 4, "Yearly" },
+                    { 3, "Monthly" },
+                    { 1, "Daily" },
+                    { 2, "Weekly" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AssetMovementDirections",
+                columns: new[] { "AssetMovementDirectionId", "AssetMovementDirectionTitle" },
+                values: new object[,]
+                {
+                    { 1, "CheckOut" },
+                    { 2, "CheckIn" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AssetStatuses",
+                columns: new[] { "AssetStatusId", "AssetStatusTitle" },
+                values: new object[,]
+                {
+                    { 7, "Sold" },
+                    { 9, "InMaimtainance" },
+                    { 8, "Broken" },
+                    { 6, "Leased" },
+                    { 2, "CheckedOut" },
+                    { 4, "Lost" },
+                    { 3, "InRepair" },
+                    { 1, "CheckedIn(Avaliable)" },
+                    { 5, "Disposed" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Months",
+                columns: new[] { "MonthId", "MonthTitle" },
+                values: new object[,]
+                {
+                    { 12, "December" },
+                    { 11, "November" },
+                    { 10, "October" },
+                    { 9, "September" },
+                    { 7, "July" },
+                    { 8, "August" },
+                    { 5, "May" },
+                    { 4, "April" },
+                    { 3, "March" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Months",
+                columns: new[] { "MonthId", "MonthTitle" },
+                values: new object[,]
+                {
+                    { 2, "February" },
+                    { 1, "January" },
+                    { 6, "June" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "WeekDays",
+                columns: new[] { "WeekDayId", "WeekDayTitle" },
+                values: new object[,]
+                {
+                    { 6, "Thursday" },
+                    { 1, "Saturday" },
+                    { 2, "Sunday" },
+                    { 3, "Monday" },
+                    { 4, "Tuesday" },
+                    { 5, "Wednesday" },
+                    { 7, "Friday" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "ActionLogs",
+                keyColumn: "ActionLogId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "AssetMaintainanceFrequencies",
+                keyColumn: "AssetMaintainanceFrequencyId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AssetMaintainanceFrequencies",
+                keyColumn: "AssetMaintainanceFrequencyId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "AssetMaintainanceFrequencies",
+                keyColumn: "AssetMaintainanceFrequencyId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "AssetMaintainanceFrequencies",
+                keyColumn: "AssetMaintainanceFrequencyId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "AssetMovementDirections",
+                keyColumn: "AssetMovementDirectionId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AssetMovementDirections",
+                keyColumn: "AssetMovementDirectionId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "AssetStatuses",
+                keyColumn: "AssetStatusId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AssetStatuses",
+                keyColumn: "AssetStatusId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "AssetStatuses",
+                keyColumn: "AssetStatusId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "AssetStatuses",
+                keyColumn: "AssetStatusId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "AssetStatuses",
+                keyColumn: "AssetStatusId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "AssetStatuses",
+                keyColumn: "AssetStatusId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "AssetStatuses",
+                keyColumn: "AssetStatusId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "AssetStatuses",
+                keyColumn: "AssetStatusId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "AssetStatuses",
+                keyColumn: "AssetStatusId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Months",
+                keyColumn: "MonthId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "WeekDays",
+                keyColumn: "WeekDayId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "WeekDays",
+                keyColumn: "WeekDayId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "WeekDays",
+                keyColumn: "WeekDayId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "WeekDays",
+                keyColumn: "WeekDayId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "WeekDays",
+                keyColumn: "WeekDayId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "WeekDays",
+                keyColumn: "WeekDayId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "WeekDays",
+                keyColumn: "WeekDayId",
+                keyValue: 7);
+        }
+    }
+}
