52594c33f9e7e563a4450da6de440c1e4a08f363
shenoudak <shenoudakaram32@gmail.com>
2022-05-09 23:42:42 +0200

    PatchMaint&&Dispose&&Leasing

--- Areas/Admin/Pages/PatchProcess/PatchMaint.cshtml.cs
+++ Areas/Admin/Pages/PatchProcess/PatchMaint.cshtml.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 using NToastNotify;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace AssetProject.Areas.Admin.Pages.PatchProcess
 {
@@ -37,6 +38,10 @@ namespace AssetProject.Areas.Admin.Pages.PatchProcess
 
             if (ModelState.IsValid)
             {
+               
+                string DueDate = assetMaintainance.AssetMaintainanceDueDate.ToString("dd/M/yyyy", CultureInfo.InvariantCulture);
+                string CompletedDate = assetMaintainance.AssetMaintainanceDateCompleted.ToString("dd/M/yyyy", CultureInfo.InvariantCulture);
+
                 if (SelectedAssets.Count != 0)
                 {
                     foreach (var asset in SelectedAssets)
@@ -44,12 +49,41 @@ namespace AssetProject.Areas.Admin.Pages.PatchProcess
 
                         AssetMaintainance assetMaintainanceObj = new AssetMaintainance
                         {
-                           
+                            AssetMaintainanceDetails = assetMaintainance.AssetMaintainanceDetails,
+                            AssetMaintainanceDateCompleted = assetMaintainance.AssetMaintainanceDateCompleted,
+                            AssetMaintainanceDueDate = assetMaintainance.AssetMaintainanceDueDate,
+                            AssetMaintainanceFrequencyId = assetMaintainance.AssetMaintainanceFrequencyId,
+                            MaintainanceStatusId = assetMaintainance.MaintainanceStatusId,
+                            AssetMaintainanceRepairesCost = assetMaintainance.AssetMaintainanceRepairesCost,
+                            AssetMaintainanceTitle = assetMaintainance.AssetMaintainanceTitle,
+                            MonthId = assetMaintainance.MonthId,
+                            AssetMaintainanceId = assetMaintainance.AssetMaintainanceId,
+                            WeekDayId = assetMaintainance.WeekDayId,
+                            WeeklyPeriod = assetMaintainance.WeeklyPeriod,
+                            MonthlyPeriod = assetMaintainance.MonthlyPeriod,
+                            AssetMaintainanceRepeating = assetMaintainance.AssetMaintainanceRepeating,
+                            MonthlyDay = assetMaintainance.MonthlyDay,
+                            YearlyDay = assetMaintainance.YearlyDay,
                             AssetId = asset.AssetId
+
                         };
+                        asset.AssetStatusId = 9;
+                        var UpdatedAsset = _context.Assets.Attach(asset);
+                        UpdatedAsset.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                        
+                        AssetLog assetLog = new AssetLog()
+                        {
+                            ActionLogId = 15,
+                            AssetId = asset.AssetId,
+                            ActionDate = DateTime.Now,
+                            Remark = string.Format($"Maintainance Asset with Title {assetMaintainance.AssetMaintainanceTitle} and DueDate {DueDate} and Completed Date {CompletedDate}")
+                        };
+                        _context.AssetLogs.Add(assetLog);
                         _context.AssetMaintainances.Add(assetMaintainanceObj);
                     }
-                    try
+
+                
+                try
                     {
                         _context.SaveChanges();
                     }
