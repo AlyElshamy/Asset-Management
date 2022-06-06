6cacf241338c2a669264d8f1e9fe917d6a118ac6
AyaMostafa5 <ayamostafa5@gmail.com>
2022-06-06 18:58:42 +0200

    update

--- Areas/Admin/Pages/SetUp/CategoryList.cshtml.cs
+++ Areas/Admin/Pages/SetUp/CategoryList.cshtml.cs
@@ -1,14 +1,47 @@
+using AssetProject.Data;
+using AssetProject.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NToastNotify;
+using System;
+using System.Linq;
 
 namespace AssetProject.Areas.Admin.Pages.SetUp
 {
     [Authorize]
     public class CategoryListModel : PageModel
     {
+        AssetContext Context;
+        private readonly IToastNotification _toastNotification;
+        public CategoryListModel(AssetContext context, IToastNotification toastNotification)
+        {
+            Context = context;
+            _toastNotification = toastNotification;
+   
+        }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPostDeleteCategory(Category cat )
+        {
+            Category DeletedCat = Context.Categories.FirstOrDefault(e=>e.CategoryId==cat.CategoryId);
+            Context.Categories.Remove(DeletedCat);
+            try
+            {
+                Context.SaveChanges();
+                _toastNotification.AddSuccessToastMessage("Category Deleted Succeffully");
+               
+            }
+            catch (Exception e)
+            {
+                _toastNotification.AddErrorToastMessage("Some Thing Went Error");
+           
+            }
+            return RedirectToPage("/SetUp/CategoryList");
+
+
         }
     }
 }
